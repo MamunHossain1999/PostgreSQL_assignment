1e460d6d0e3d78246bca80e3cff00b668dd5f3f4
Your Name <youremail@example.com>
2025-05-25 18:35:21 +0600

    added create table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -15,6 +15,8 @@ INSERT INTO rangers (name, region) VALUES
 ('Vikram', 'Central Zone'),
 ('Anita', 'East Zone');
 
+DELETE FROM rangers;
+
 SELECT * from rangers;
 
 CREATE TABLE species (
@@ -45,5 +47,10 @@ CREATE TABLE sightings (
     notes TEXT
 );
 
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
 
 SELECT * FROM sightings;
